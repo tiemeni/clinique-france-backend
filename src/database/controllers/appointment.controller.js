05bb265ddf2dc5b083a805de1444678153a9ac81
tiemeni <tiemanirocket@gmail.com>
2023-12-10 03:50:12 +0100

    update rdv post

--- src/database/controllers/appointment.controller.js
+++ src/database/controllers/appointment.controller.js
@@ -83,6 +83,7 @@ const makeAppointment = async (req, res) => {
       startTime: data.startTime,
       practitioner: data.practitioner,
     });
+    console.log(isExist)
     if (isExist)
       return handler.errorHandler(
         res,
@@ -109,9 +110,8 @@ const makeAppointment = async (req, res) => {
       type: notificationType.APPOINTMENT_CREATED,
     });
     // Get user informations
-    const { user } = await findUserByFiche(data.patient);
-
-    io.to(user._id.toString()).emit("notification", notification);
+    //const { user } = await findUserByFiche(data.patient);
+    //io.to(user._id.toString()).emit("notification", notification);
     return handler.successHandler(res, rdv, httpStatus.CREATED);
   } catch (error) {
     return handler.errorHandler(
