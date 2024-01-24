fe3093eb0ae6f830027c76b13bf62796ae5e9421
tiemeni <tiemanirocket@gmail.com>
2024-01-24 11:01:01 +0100

    update'

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ require("dotenv").config();
 
 server.use(
   cors({
-    origin: ["http://10.10.90.10:3000", "http://10.10.90.60:3000", "http://10.10.90.11:3000", "http://10.10.90.60:3000", "http://10.10.90.17:3000","http://192.168.43.35:3000", "http://localhost:3000", "http://10.10.90.239:3000"],
+    origin: ["http://10.10.90.10:3000","https://clinique-france-frontend.vercel.app", "http://10.10.90.60:3000", "http://10.10.90.11:3000", "http://10.10.90.60:3000", "http://10.10.90.17:3000","http://192.168.43.35:3000", "http://localhost:3000", "http://10.10.90.239:3000"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     preflightContinue: true,
     allowedHeaders: ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin'],
