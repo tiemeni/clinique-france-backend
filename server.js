c3d55ca738327b919a4571ca9efeb1692800c143
tiemeni <tiemanirocket@gmail.com>
2024-01-24 11:56:30 +0100

    update

--- server.js
+++ server.js
@@ -74,7 +74,9 @@ server.use(cookieParser());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://clinique-france-frontend.vercel.app")
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 server.use((req, res, next) => {
