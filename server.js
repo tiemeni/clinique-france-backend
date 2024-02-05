97c1d9281f774f0f397565c86e0f17d8da06f163
tiemeni <tiemanirocket@gmail.com>
2024-02-05 10:46:15 +0100

    update

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK'
+    })
+  }
   next();
 });
 server.use((req, res, next) => {
