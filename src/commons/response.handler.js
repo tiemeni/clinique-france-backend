5bdc8bc68a86ee66353a70a4d4181736278e4ccd
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-05-23 14:20:52 +0100

    check for verify token on back ver completed

--- src/commons/response.handler.js
+++ src/commons/response.handler.js
@@ -5,8 +5,8 @@ exports.successHandler = (res, data, statusCode = 200) => {
     if(data?.access_token) res.cookie(COOKIE_NAME, data.access_token, {
         maxAge: env.EXPIRE_DATE,
         httpOnly: true,
-        secure: true, // Utilisez 'secure' si vous êtes en production et utilisez HTTPS
-        sameSite: 'None'
+        //secure: true, // 
+        sameSite: 'Lax'
       });
     res.status(statusCode).json({ success: true, data });
 }
