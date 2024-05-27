1d95f5a3acaeea654d99dbe5c190aa32374dd9b7
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-05-27 12:16:37 +0100

    fix online logout err ver 2

--- src/routes/disconnectUser.js
+++ src/routes/disconnectUser.js
@@ -2,10 +2,9 @@ const { COOKIE_NAME } = require("../commons/constants")
 const { successHandler } = require("../commons/response.handler")
 
 module.exports.disconnectUser = async (req, res) => {
-        req
         res.cookie(COOKIE_NAME, '', {
-                httpOnly: process.env.NODE_ENV === 'production',
-                secure: process.env.NODE_ENV === 'production', 
+                httpOnly: process.env.NODE_ENV === 'production' | true,
+                secure: process.env.NODE_ENV === 'production' | true, 
                 expires: new Date(0) 
               });
         successHandler(res, {message: "processing disconnexion"}, 201)
