c89578b4063f54f36750202a53c64fa64559049f
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-05-27 12:04:25 +0100

    fix online  logout ver 1

--- src/commons/response.handler.js
+++ src/commons/response.handler.js
@@ -4,8 +4,8 @@ const { COOKIE_NAME } = require("./constants");
 exports.successHandler = (res, data, statusCode = 200) => {
     if(data?.access_token) res.cookie(COOKIE_NAME, data.access_token, {
         maxAge: env.EXPIRE_DATE,
-        httpOnly: process.env.NODE_ENV === 'production',
-        secure: process.env.NODE_ENV === 'production', 
+        httpOnly: process.env.NODE_ENV === 'production' | true,
+        secure: process.env.NODE_ENV === 'production' | true, 
         sameSite: 'None'
       });
     res.status(statusCode).json({ success: true, data });
