41c858e53ab84733d7ed3439011b020ec4fa80af
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2024-05-27 11:42:19 +0100

    problème des cookies résolu - connexion et déconnexion quasi ok - pour utiliser l'app en dev il faut juste supprimer le dossier api et commenter la dernière ligne de server.js (export default ...) et ensuite mettre la variable NODE_ENV à "developpement"

--- src/commons/response.handler.js
+++ src/commons/response.handler.js
@@ -4,8 +4,8 @@ const { COOKIE_NAME } = require("./constants");
 exports.successHandler = (res, data, statusCode = 200) => {
     if(data?.access_token) res.cookie(COOKIE_NAME, data.access_token, {
         maxAge: env.EXPIRE_DATE,
-        httpOnly: true,
-        secure: true, 
+        httpOnly: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production', 
         sameSite: 'None'
       });
     res.status(statusCode).json({ success: true, data });
--- src/routes/disconnectUser.js
+++ src/routes/disconnectUser.js
@@ -2,6 +2,11 @@ const { COOKIE_NAME } = require("../commons/constants")
 const { successHandler } = require("../commons/response.handler")
 
 module.exports.disconnectUser = async (req, res) => {
-        res.cookie(COOKIE_NAME, "", null)
+        req
+        res.cookie(COOKIE_NAME, '', {
+                httpOnly: process.env.NODE_ENV === 'production',
+                secure: process.env.NODE_ENV === 'production', 
+                expires: new Date(0) 
+              });
         successHandler(res, {message: "processing disconnexion"}, 201)
 }
